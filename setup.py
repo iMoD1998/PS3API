53cf494d23d9cffe2567f823f01f26e2c7e6285c
Adam <imod1998@outlook.com>
2021-12-29 21:51:13 +0000

    Add setup info.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ps3api',
+      version='0.1',
+      description='Python module for interacting with PS3 using CCAPI or TMAPI.',
+      url='http://github.com/iMoD1998/PS3API',
+      author='iMoD1998',
+      author_email='imod1998@protonmail.com',
+      license='MIT',
+      packages=['ps3api'],
+      zip_safe=False)
