1b2b018d772f17fad612b9c6c2092f90233bf155
Adam <imod1998@outlook.com>
2021-12-31 07:05:03 +0000

    Additions and improvements to RPC backend.
    * Add new calling methods.
    * Add system call support.
    * Cleanup and documentation.

--- ps3api/RPCPayload.s
+++ ps3api/RPCPayload.s
@@ -1,4 +1,37 @@
-.set STACK_SIZE, 0x200
+.set STACK_SIZE, 0x220
+
+#
+# Layout for call context structure.
+#
+.set CALL_CONTEXT_R3,           0x00
+.set CALL_CONTEXT_R4,           CALL_CONTEXT_R3 + 0x8
+.set CALL_CONTEXT_R5,           CALL_CONTEXT_R4 + 0x8
+.set CALL_CONTEXT_R6,           CALL_CONTEXT_R5 + 0x8
+.set CALL_CONTEXT_R7,           CALL_CONTEXT_R6 + 0x8
+.set CALL_CONTEXT_R8,           CALL_CONTEXT_R7 + 0x8
+.set CALL_CONTEXT_R9,           CALL_CONTEXT_R8 + 0x8
+.set CALL_CONTEXT_R10,          CALL_CONTEXT_R9 + 0x8
+.set CALL_CONTEXT_R11,          CALL_CONTEXT_R10 + 0x8
+.set CALL_CONTEXT_F1,           CALL_CONTEXT_R11 + 0x8
+.set CALL_CONTEXT_F2,           CALL_CONTEXT_F1 + 0x4
+.set CALL_CONTEXT_F3,           CALL_CONTEXT_F2 + 0x4
+.set CALL_CONTEXT_F4,           CALL_CONTEXT_F3 + 0x4
+.set CALL_CONTEXT_F5,           CALL_CONTEXT_F4 + 0x4
+.set CALL_CONTEXT_F6,           CALL_CONTEXT_F5 + 0x4
+.set CALL_CONTEXT_F7,           CALL_CONTEXT_F6 + 0x4
+.set CALL_CONTEXT_F8,           CALL_CONTEXT_F7 + 0x4
+.set CALL_CONTEXT_F9,           CALL_CONTEXT_F8 + 0x4
+.set CALL_CONTEXT_CALL_TYPE,    CALL_CONTEXT_F9 + 0x4
+.set CALL_CONTEXT_CALL_ADDRESS, CALL_CONTEXT_CALL_TYPE + 0x4
+.set CALL_CONTEXT_CALL_TOC,     CALL_CONTEXT_CALL_ADDRESS + 0x8
+.set CALL_CONTEXT_RETURN_R3,    CALL_CONTEXT_CALL_TOC  + 0x8
+.set CALL_CONTEXT_RETURN_F1,    CALL_CONTEXT_RETURN_R3 + 0x8
+
+.set CALL_TYPE_NONE, 0x00
+.set CALL_TYPE_FUNCTION, 0x01
+.set CALL_TYPE_FUNCTION_SET_TOC, 0x02
+.set CALL_TYPE_FUNCTION_OPD_ENTRY, 0x03
+.set CALL_TYPE_SYS_CALL, 0x04
 
 # 
 # Save link register.
@@ -9,6 +42,7 @@ std   %r0,  -0x8(%r1)
 #
 # Backup nonvolatile registers
 #
+std   %r28, -0x28(%r1)
 std   %r29, -0x20(%r1)
 std   %r30, -0x18(%r1)
 std   %r31, -0x10(%r1)
@@ -27,91 +61,143 @@ ori   %r31, %r31, 0xCAFEBEEF@l
 #
 # Check if we have a function to call.
 #
-lwz   %r30, 0x70(%r31)
+ld    %r30, CALL_CONTEXT_CALL_ADDRESS(%r31)
 cmpwi %r30, 0
 beq   _RPCEnd
 
 #
 # Backup hooked function args.
 #
-stfd  %f1,  STACK_SIZE-0x0110(%r1)
-stfd  %f2,  STACK_SIZE-0x0108(%r1)
-stfd  %f3,  STACK_SIZE-0x0100(%r1)
-stfd  %f4,  STACK_SIZE-0x0098(%r1)
-stfd  %f5,  STACK_SIZE-0x0090(%r1)
-stfd  %f6,  STACK_SIZE-0x0088(%r1)
-stfd  %f7,  STACK_SIZE-0x0080(%r1)
-stfd  %f8,  STACK_SIZE-0x0078(%r1)
-stfd  %f9,  STACK_SIZE-0x0070(%r1)
-std   %r3,  STACK_SIZE-0x0068(%r1)
-std   %r4,  STACK_SIZE-0x0060(%r1)
-std   %r5,  STACK_SIZE-0x0058(%r1)
-std   %r6,  STACK_SIZE-0x0050(%r1)
-std   %r7,  STACK_SIZE-0x0048(%r1)
-std   %r8,  STACK_SIZE-0x0040(%r1)
-std   %r9,  STACK_SIZE-0x0038(%r1)
-std   %r10, STACK_SIZE-0x0030(%r1)
-std   %r11, STACK_SIZE-0x0028(%r1)
+stfd  %f1,  STACK_SIZE-0x0118(%r1)
+stfd  %f2,  STACK_SIZE-0x0110(%r1)
+stfd  %f3,  STACK_SIZE-0x0108(%r1)
+stfd  %f4,  STACK_SIZE-0x0100(%r1)
+stfd  %f5,  STACK_SIZE-0x0098(%r1)
+stfd  %f6,  STACK_SIZE-0x0090(%r1)
+stfd  %f7,  STACK_SIZE-0x0088(%r1)
+stfd  %f8,  STACK_SIZE-0x0080(%r1)
+stfd  %f9,  STACK_SIZE-0x0078(%r1)
+std   %r3,  STACK_SIZE-0x0070(%r1)
+std   %r4,  STACK_SIZE-0x0068(%r1)
+std   %r5,  STACK_SIZE-0x0060(%r1)
+std   %r6,  STACK_SIZE-0x0058(%r1)
+std   %r7,  STACK_SIZE-0x0050(%r1)
+std   %r8,  STACK_SIZE-0x0048(%r1)
+std   %r9,  STACK_SIZE-0x0040(%r1)
+std   %r10, STACK_SIZE-0x0038(%r1)
+std   %r11, STACK_SIZE-0x0030(%r1)
 
 
 #
 # Load registers with arguments.
 #
-ld    %r3,  0x00(%r31)
-ld    %r4,  0x08(%r31)
-ld    %r5,  0x10(%r31)
-ld    %r6,  0x18(%r31)
-ld    %r7,  0x20(%r31)
-ld    %r8,  0x28(%r31)
-ld    %r9,  0x30(%r31)
-ld    %r10, 0x38(%r31)
-ld    %r11, 0x40(%r31)
-lfs   %f1,  0x48(%r31)
-lfs   %f2,  0x4C(%r31)
-lfs   %f3,  0x50(%r31)
-lfs   %f4,  0x54(%r31)
-lfs   %f5,  0x58(%r31)
-lfs   %f6,  0x5C(%r31)
-lfs   %f7,  0x60(%r31)
-lfs   %f8,  0x64(%r31)
-lfs   %f9,  0x68(%r31)
+ld    %r3,  CALL_CONTEXT_R3(%r31)
+ld    %r4,  CALL_CONTEXT_R4(%r31)
+ld    %r5,  CALL_CONTEXT_R5(%r31)
+ld    %r6,  CALL_CONTEXT_R6(%r31)
+ld    %r7,  CALL_CONTEXT_R7(%r31)
+ld    %r8,  CALL_CONTEXT_R8(%r31)
+ld    %r9,  CALL_CONTEXT_R9(%r31)
+ld    %r10, CALL_CONTEXT_R10(%r31)
+ld    %r11, CALL_CONTEXT_R11(%r31)
+lfs   %f1,  CALL_CONTEXT_F1(%r31)
+lfs   %f2,  CALL_CONTEXT_F2(%r31)
+lfs   %f3,  CALL_CONTEXT_F3(%r31)
+lfs   %f4,  CALL_CONTEXT_F4(%r31)
+lfs   %f5,  CALL_CONTEXT_F5(%r31)
+lfs   %f6,  CALL_CONTEXT_F6(%r31)
+lfs   %f7,  CALL_CONTEXT_F7(%r31)
+lfs   %f8,  CALL_CONTEXT_F8(%r31)
+lfs   %f9,  CALL_CONTEXT_F9(%r31)
+
+#
+# Check which call type we have.
+#
+lwz   %r29, CALL_CONTEXT_CALL_TYPE(%r31)
+
+#
+# Standard call to function pointer in the call context.
+#
+_CheckCall:
+cmpwi %r29, CALL_TYPE_FUNCTION
+bne   _CheckCallTOC
+mr    %r28, %r2
+b     _CallWithTOC
+
+#
+# Standard call to function pointer while using the TOC provided in the call context.
+#
+_CheckCallTOC:
+cmpwi %r29, CALL_TYPE_FUNCTION_SET_TOC
+bne   _CheckCallOpd
+ld    %r28, CALL_CONTEXT_CALL_TOC(%r31)
+b     _CallWithTOC
+
+#
+# Call to OPD entry pointer which contains the function address and TOC.
+#
+# Example entry:
+# OPDEntry <sub_258460, 0x724C38>
+#
+_CheckCallOpd:
+cmpwi %r29, CALL_TYPE_FUNCTION_OPD_ENTRY
+bne   _CheckCallSysCall
+lwz   %r28, 4(%r30)
+lwz   %r30, 0(%r30)
+b     _CallWithTOC
+
+#
+# Call system call with the system call index provided in r11
+# 
+_CheckCallSysCall:
+cmpwi %r29, CALL_TYPE_SYS_CALL
+bne  _PostCallNoReturn
+sc
+b    _PostCall
+
+_CallWithTOC:
+std   %r2,  STACK_SIZE-0x0120(%r1)
+mr    %r2,  %r28
 mtctr %r30
-bctrl # call
+bctrl # Call Function
+ld    %r2,  STACK_SIZE-0x0120(%r1)
 
 #
 # Store return values.
 #
-std   %r3,  0x78(%r31)
-stfs  %f1,  0x80(%r31)
+_PostCall:
+std   %r3,  CALL_CONTEXT_RETURN_R3(%r31)
+stfs  %f1,  CALL_CONTEXT_RETURN_F1(%r31)
 
 #
 # Set call address to 0.
 # Used to determine externally if function has executed.
 #
+_PostCallNoReturn:
 xor   %r30, %r30, %r30
-stw   %r30, 0x70(%r31)
+std   %r30, CALL_CONTEXT_CALL_ADDRESS(%r31)
 
 #
 # Restore hooked function args.
 #
-lfd   %f1,  STACK_SIZE-0x0110(%r1)
-lfd   %f2,  STACK_SIZE-0x0108(%r1)
-lfd   %f3,  STACK_SIZE-0x0100(%r1)
-lfd   %f4,  STACK_SIZE-0x0098(%r1)
-lfd   %f5,  STACK_SIZE-0x0090(%r1)
-lfd   %f6,  STACK_SIZE-0x0088(%r1)
-lfd   %f7,  STACK_SIZE-0x0080(%r1)
-lfd   %f8,  STACK_SIZE-0x0078(%r1)
-lfd   %f9,  STACK_SIZE-0x0070(%r1)
-ld    %r3,  STACK_SIZE-0x0068(%r1)
-ld    %r4,  STACK_SIZE-0x0060(%r1)
-ld    %r5,  STACK_SIZE-0x0058(%r1)
-ld    %r6,  STACK_SIZE-0x0050(%r1)
-ld    %r7,  STACK_SIZE-0x0048(%r1)
-ld    %r8,  STACK_SIZE-0x0040(%r1)
-ld    %r9,  STACK_SIZE-0x0038(%r1)
-ld    %r10, STACK_SIZE-0x0030(%r1)
-ld    %r11, STACK_SIZE-0x0028(%r1)
+lfd  %f1,  STACK_SIZE-0x0118(%r1)
+lfd  %f2,  STACK_SIZE-0x0110(%r1)
+lfd  %f3,  STACK_SIZE-0x0108(%r1)
+lfd  %f4,  STACK_SIZE-0x0100(%r1)
+lfd  %f5,  STACK_SIZE-0x0098(%r1)
+lfd  %f6,  STACK_SIZE-0x0090(%r1)
+lfd  %f7,  STACK_SIZE-0x0088(%r1)
+lfd  %f8,  STACK_SIZE-0x0080(%r1)
+lfd  %f9,  STACK_SIZE-0x0078(%r1)
+ld   %r3,  STACK_SIZE-0x0070(%r1)
+ld   %r4,  STACK_SIZE-0x0068(%r1)
+ld   %r5,  STACK_SIZE-0x0060(%r1)
+ld   %r6,  STACK_SIZE-0x0058(%r1)
+ld   %r7,  STACK_SIZE-0x0050(%r1)
+ld   %r8,  STACK_SIZE-0x0048(%r1)
+ld   %r9,  STACK_SIZE-0x0040(%r1)
+ld   %r10, STACK_SIZE-0x0038(%r1)
+ld   %r11, STACK_SIZE-0x0030(%r1)
 
 #
 # Free stack.
@@ -136,6 +222,7 @@ mtctr %r31
 #
 # Restore nonvolatile registers.
 #
+ld    %r28, -0x28(%r1)
 ld    %r29, -0x20(%r1)
 ld    %r30, -0x18(%r1)
 ld    %r31, -0x10(%r1)
--- ps3api/rpc.py
+++ ps3api/rpc.py
@@ -5,37 +5,40 @@ from ctypes import _Pointer
 from .memutils import *
 
 class RPCPayload:
+    #
     # RPC shelldcode/payload that will be used to call functions (See RPCPayload.s)
+    #
     PayloadData = bytearray(
         b"\x7C\x08\x02\xA6"
         b"\xF8\x01\xFF\xF8"
+        b"\xFB\x81\xFF\xD8"
         b"\xFB\xA1\xFF\xE0"
         b"\xFB\xC1\xFF\xE8"
         b"\xFB\xE1\xFF\xF0"
-        b"\xF8\x21\xFE\x01"
+        b"\xF8\x21\xFD\xE1"
         b"\x3F\xE0\xCA\xFE"
         b"\x63\xFF\xBE\xEF"
-        b"\x83\xDF\x00\x70"
+        b"\xEB\xDF\x00\x70"
         b"\x2C\x1E\x00\x00"
-        b"\x41\x82\x00\xF4"
-        b"\xD8\x21\x00\xF0"
-        b"\xD8\x41\x00\xF8"
-        b"\xD8\x61\x01\x00"
-        b"\xD8\x81\x01\x68"
-        b"\xD8\xA1\x01\x70"
-        b"\xD8\xC1\x01\x78"
-        b"\xD8\xE1\x01\x80"
-        b"\xD9\x01\x01\x88"
-        b"\xD9\x21\x01\x90"
-        b"\xF8\x61\x01\x98"
-        b"\xF8\x81\x01\xA0"
-        b"\xF8\xA1\x01\xA8"
-        b"\xF8\xC1\x01\xB0"
-        b"\xF8\xE1\x01\xB8"
-        b"\xF9\x01\x01\xC0"
-        b"\xF9\x21\x01\xC8"
-        b"\xF9\x41\x01\xD0"
-        b"\xF9\x61\x01\xD8"
+        b"\x41\x82\x01\x48"
+        b"\xD8\x21\x01\x08"
+        b"\xD8\x41\x01\x10"
+        b"\xD8\x61\x01\x18"
+        b"\xD8\x81\x01\x20"
+        b"\xD8\xA1\x01\x88"
+        b"\xD8\xC1\x01\x90"
+        b"\xD8\xE1\x01\x98"
+        b"\xD9\x01\x01\xA0"
+        b"\xD9\x21\x01\xA8"
+        b"\xF8\x61\x01\xB0"
+        b"\xF8\x81\x01\xB8"
+        b"\xF8\xA1\x01\xC0"
+        b"\xF8\xC1\x01\xC8"
+        b"\xF8\xE1\x01\xD0"
+        b"\xF9\x01\x01\xD8"
+        b"\xF9\x21\x01\xE0"
+        b"\xF9\x41\x01\xE8"
+        b"\xF9\x61\x01\xF0"
         b"\xE8\x7F\x00\x00"
         b"\xE8\x9F\x00\x08"
         b"\xE8\xBF\x00\x10"
@@ -54,36 +57,58 @@ class RPCPayload:
         b"\xC0\xFF\x00\x60"
         b"\xC1\x1F\x00\x64"
         b"\xC1\x3F\x00\x68"
+        b"\x83\xBF\x00\x6C"
+        b"\x2C\x1D\x00\x01"
+        b"\x40\x82\x00\x0C"
+        b"\x7C\x5C\x13\x78"
+        b"\x48\x00\x00\x38"
+        b"\x2C\x1D\x00\x02"
+        b"\x40\x82\x00\x0C"
+        b"\xEB\x9F\x00\x78"
+        b"\x48\x00\x00\x28"
+        b"\x2C\x1D\x00\x03"
+        b"\x40\x82\x00\x10"
+        b"\x83\x9E\x00\x04"
+        b"\x83\xDE\x00\x00"
+        b"\x48\x00\x00\x14"
+        b"\x2C\x1D\x00\x04"
+        b"\x40\x82\x00\x28"
+        b"\x44\x00\x00\x02"
+        b"\x48\x00\x00\x18"
+        b"\xF8\x41\x01\x00"
+        b"\x7F\x82\xE3\x78"
         b"\x7F\xC9\x03\xA6"
         b"\x4E\x80\x04\x21"
-        b"\xF8\x7F\x00\x78"
-        b"\xD0\x3F\x00\x80"
+        b"\xE8\x41\x01\x00"
+        b"\xF8\x7F\x00\x80"
+        b"\xD0\x3F\x00\x88"
         b"\x7F\xDE\xF2\x78"
-        b"\x93\xDF\x00\x70"
-        b"\xC8\x21\x00\xF0"
-        b"\xC8\x41\x00\xF8"
-        b"\xC8\x61\x01\x00"
-        b"\xC8\x81\x01\x68"
-        b"\xC8\xA1\x01\x70"
-        b"\xC8\xC1\x01\x78"
-        b"\xC8\xE1\x01\x80"
-        b"\xC9\x01\x01\x88"
-        b"\xC9\x21\x01\x90"
-        b"\xE8\x61\x01\x98"
-        b"\xE8\x81\x01\xA0"
-        b"\xE8\xA1\x01\xA8"
-        b"\xE8\xC1\x01\xB0"
-        b"\xE8\xE1\x01\xB8"
-        b"\xE9\x01\x01\xC0"
-        b"\xE9\x21\x01\xC8"
-        b"\xE9\x41\x01\xD0"
-        b"\xE9\x61\x01\xD8"
-        b"\x38\x21\x02\x00"
+        b"\xFB\xDF\x00\x70"
+        b"\xC8\x21\x01\x08"
+        b"\xC8\x41\x01\x10"
+        b"\xC8\x61\x01\x18"
+        b"\xC8\x81\x01\x20"
+        b"\xC8\xA1\x01\x88"
+        b"\xC8\xC1\x01\x90"
+        b"\xC8\xE1\x01\x98"
+        b"\xC9\x01\x01\xA0"
+        b"\xC9\x21\x01\xA8"
+        b"\xE8\x61\x01\xB0"
+        b"\xE8\x81\x01\xB8"
+        b"\xE8\xA1\x01\xC0"
+        b"\xE8\xC1\x01\xC8"
+        b"\xE8\xE1\x01\xD0"
+        b"\xE9\x01\x01\xD8"
+        b"\xE9\x21\x01\xE0"
+        b"\xE9\x41\x01\xE8"
+        b"\xE9\x61\x01\xF0"
+        b"\x38\x21\x02\x20"
         b"\xE8\x01\xFF\xF8"
         b"\x7C\x08\x03\xA6"
         b"\x3F\xE0\xDE\xAD"
         b"\x63\xFF\xC0\xDE"
         b"\x7F\xE9\x03\xA6"
+        b"\xEB\x81\xFF\xD8"
         b"\xEB\xA1\xFF\xE0"
         b"\xEB\xC1\xFF\xE8"
         b"\xEB\xE1\xFF\xF0"
@@ -100,20 +125,20 @@ class RPCPayload:
     # The offset in the payload conaining the address where arguments will be loaded from.
     # 16 bits for higher and lower (lis, ori)
     #
-    ArgumentContextsHighOffset = 0x1A
-    ArgumentContextsLowOffset  = 0x1E
+    ArgumentContextsHighOffset = 0x1E
+    ArgumentContextsLowOffset  = 0x22
 
     #
     # The offsets in the payload for setting the address to branch back to after the hook.
     # 16 bits for higher and lower (lis, ori)
     #
-    BranchBackHighOffset       = 0x12A
-    BranchBackLowOffset        = 0x12E
+    BranchBackHighOffset       = 0x182
+    BranchBackLowOffset        = 0x186
 
     #
     # The offset for storing instructions that need to be executed before branching back to the original.
     #
-    OriginalInstructionsOffset = 0x140
+    OriginalInstructionsOffset = 0x19C
 
     def SetPayloadData(Offset, Data):
         RPCPayload.PayloadData[Offset:Offset+len(Data)] = Data
@@ -159,11 +184,19 @@ RPCCTypeBasicTypes = {
 }
 
 class RPCCallContext:
+    #
+    # Call context offsets and info (see RPCPayload.s)
+    #
+    NumberGPRegs = 9
+    NumberFPRegs = 9
+
     GPRegisterArrayOffset = 0x00
-    FPRegisterArrayOffset = 0x48
-    CallAddressOffset     = 0x70
-    ReturnGPOffset        = 0x78
-    ReturnFPOffset        = 0x80
+    FPRegisterArrayOffset = GPRegisterArrayOffset + (8 * NumberGPRegs)
+    CallTypeOffset        = FPRegisterArrayOffset + (4 * NumberFPRegs)
+    CallAddressOffset     = CallTypeOffset + 4
+    CallTOCOffset         = CallAddressOffset + 8
+    ReturnGPOffset        = CallTOCOffset + 8
+    ReturnFPOffset        = ReturnGPOffset + 8
 
     def GetGPRegisterOffset(RegisterIndex):
         return RPCCallContext.GPRegisterArrayOffset + (0x8 * RegisterIndex)
@@ -179,8 +212,14 @@ class RPCCallContext:
         self.ArgumentIntIndex   = 0
         self.ArgumentFloatIndex = 0
     
+    def SetCallType(self, Address):
+        self.ArgumentsBuffer[RPCCallContext.CallTypeOffset:RPCCallContext.CallTypeOffset+4] = PackInt32BE(Address)
+
+    def SetCallTOCAddress(self, Address):
+        self.ArgumentsBuffer[RPCCallContext.CallTOCOffset:RPCCallContext.CallTOCOffset+8] = PackInt64BE(Address)
+    
     def SetCallAddress(self, Address):
-        self.ArgumentsBuffer[RPCCallContext.CallAddressOffset:RPCCallContext.CallAddressOffset+4] = PackInt32BE(Address)
+        self.ArgumentsBuffer[RPCCallContext.CallAddressOffset:RPCCallContext.CallAddressOffset+8] = PackInt64BE(Address)
 
     def SetGPRegister(self, RegIndex, Value):
         RegOffset = RPCCallContext.GetGPRegisterOffset(RegIndex)
@@ -212,55 +251,88 @@ class RPCCallContext:
 from pwn import *
 
 class RPCFunction:
-    def __init__(self, API, Address):
-        self.address  = Address
-        self.argtypes = None
-        self.restype  = None
-        self.API      = API
+    #
+    # Call types see (RPCPayload.s)
+    #
+    CALL_TYPE_NONE               = 0x00
+    CALL_TYPE_FUNCTION           = 0x01
+    CALL_TYPE_FUNCTION_SET_TOC   = 0x02
+    CALL_TYPE_FUNCTION_OPD_ENTRY = 0x03
+    CALL_TYPE_SYS_CALL           = 0x04
+
+    def __init__(self, API, Address, TOCAddress = 0, Type=CALL_TYPE_FUNCTION):
+        self.Address     = Address
+        self.TOCAddress  = TOCAddress
+        self.ArgTypes    = None
+        self.ReturnType  = None
+        self.CallType    = Type
+        self.API         = API
 
     def __call__(self, *Args):
         ArgCount    = len(Args)
         CallContext = RPCCallContext(RPC.FreeSpaceAddress, RPC.ArgDataSpaceAddress)
 
-        if ArgCount != len(self.argtypes):
-            raise Exception('Wrong number of args expected: ' + str(len(self.argtypes)))
+        if ArgCount != len(self.ArgTypes):
+            raise Exception('Wrong number of args expected: ' + str(len(self.ArgTypes)))
 
         for i in range(0, ArgCount):
-            ArgType  = self.argtypes[i]
+            ArgType  = self.ArgTypes[i]
             ArgValue = Args[i]
             
+            #
             # TODO: Support more complicated types, structures etc.
-            if issubclass(self.argtypes[i], _SimpleCData):
-                RPCCTypeBasicTypes[self.argtypes[i]](CallContext, ArgValue)
+            #
+            if issubclass(self.ArgTypes[i], _SimpleCData):
+                RPCCTypeBasicTypes[self.ArgTypes[i]](CallContext, ArgValue)
             #elif issubclass(self.argtypes[i], _Pointer):
 
-        # Set the function address to call in the payload.
-        CallContext.SetCallAddress(self.address)
+        #
+        # If we are doing a syscall set r11 to the syscall index.
+        #
+        if self.CallType == RPCFunction.CALL_TYPE_SYS_CALL:
+            CallContext.SetCallAddress(0xDEADBEEF)
+            CallContext.SetGPRegister(8, self.Address)
+        else:
+            CallContext.SetCallTOCAddress(self.TOCAddress)
+            CallContext.SetCallAddress(self.Address)
+
+        CallContext.SetCallType(self.CallType)
 
         FinalArgumentData = CallContext.ArgumentsBuffer + CallContext.ArgumentsData
 
         print(hexdump(FinalArgumentData))
 
-        # Write payload
+        #
+        # Write call context.
+        #
         self.API.WriteMemory(RPC.FreeSpaceAddress, FinalArgumentData)
 
-        # Wait for function to execute
-        while self.API.ReadInt32(RPC.FreeSpaceAddress + 0x70) != 0:
+        #
+        # Wait for function to execute.
+        #
+        while self.API.ReadInt32(RPC.FreeSpaceAddress + RPCCallContext.CallAddressOffset) != 0:
             pass
 
+        #
         # TODO: Support complicated return types??
-        if self.restype:           
-            if self.restype == c_float:
-                return self.API.ReadFloat(RPC.FreeSpaceAddress + 0x80)
+        #
+        if self.ReturnType:           
+            if self.ReturnType == c_float:
+                return self.API.ReadFloat(RPC.FreeSpaceAddress + RPCCallContext.ReturnFPOffset)
             else:
-                return self.API.ReadInt64(RPC.FreeSpaceAddress + 0x78)
+                return self.API.ReadInt64(RPC.FreeSpaceAddress + RPCCallContext.ReturnGPOffset)
         
         return None
 
 class RPC:
+    #
     # Free space to write function arguments.
+    #
     FreeSpaceAddress    = 0x10051000
+
+    #
     # Extra space to write arguments that dont fit in a register ie. arrays, strings etc..
+    #
     ArgDataSpaceAddress = FreeSpaceAddress + 0x100
 
     def __init__(self, API):
@@ -269,29 +341,41 @@ class RPC:
         self.OriginalInstructions = bytearray()
     
     def Enable(self, HookAddress):
+        #
         # bla 0x10000
+        #
         HookPatch             = b'\x48\x01\x00\x02'
         HookAddressAfterPatch = HookAddress + len(HookPatch)
 
+        #
         # Check if we are already hooked.
+        #
         if self.API.ReadMemory(HookAddress, 4) == HookPatch:
             return
 
         # Set the address to read arguments from (registers)
         RPCPayload.SetArgumentContextsAddress(RPC.FreeSpaceAddress)
 
+        #
         # Set the address to jump back to after our RPC payload has ran.
+        #
         RPCPayload.SetBranchBackAddress(HookAddressAfterPatch)
 
+        #
         # Get original instructions
+        #
         self.OriginalInstructions = self.API.ReadMemory(HookAddress, len(HookPatch))
 
         RPCPayload.SetOriginalInstructions(self.OriginalInstructions)
 
+        #
         # Write payload at ELF header because retarded page prot
+        #
         self.API.WriteMemory(0x10000, RPCPayload.PayloadData)
 
+        #
         # Write branch for hook.
+        #
         self.API.WriteMemory(HookAddress, HookPatch)
 
     def Disable():
